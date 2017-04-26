f8e3f2baec6af1a46c0b0d43d505f6103d775167
agnostics <brandilla1337@gmail.com>
2017-04-26 13:00:12 -0400

    Initial init

---
+++ src/App.js
@@ -0,0 +1,242 @@
+import React, {Component} from 'react';
+//import logo from './logo.svg';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <div className="table">
+
+          {/* Row 1 */}
+          <div className="row">
+            <div className="element polyatomic-nonmetal">
+              <div className="atomic-number">1</div>
+              <abbr>H</abbr>
+            </div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element"></div>
+          </div>
+
+          {/* Row 2 */}
+          <div className="row">
+            <div className="element alkali-metal"></div>
+            <div className="element alkaline-earth"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+          </div>
+
+          {/* Row 3 */}
+          <div className="row">
+            <div className="element alkali-metal"></div>
+            <div className="element alkaline-earth"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+          </div>
+
+          {/* Row 4 */}
+          <div className="row">
+            <div className="element alkali-metal"></div>
+            <div className="element alkaline-earth"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+          </div>
+
+          {/* Row 5 */}
+          <div className="row">
+            <div className="element alkali-metal"></div>
+            <div className="element alkaline-earth"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+          </div>
+
+          {/* Row 6 */}
+          <div className="row">
+            <div className="element alkali-metal"></div>
+            <div className="element alkaline-earth"></div>
+            <div className="element blank"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+          </div>
+
+          {/* Row 7 */}
+          <div className="row">
+            <div className="element alkali-metal"></div>
+            <div className="element alkaline-earth"></div>
+            <div className="element blank"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element transition-metal"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+            <div className="element"></div>
+          </div>
+
+          {/* Buffer*/}
+
+          <div className="row">
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+            <div className="element blank-half"></div>
+          </div>
+
+          {/* Row 8 */}
+          <div className="row">
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+            <div className="element lanthanide"></div>
+          </div>
+
+          {/* Row 9 */}
+          <div className="row">
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element blank"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+            <div className="element actinide"></div>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.scss';
+
+
+const rootEl = document.getElementById('root')
+
+ReactDOM.render(
+  <App />,
+  rootEl
+)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    )
+  })
+}
